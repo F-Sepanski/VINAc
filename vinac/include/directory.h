9e88290d43fb3a154922d26ca4d982c18346f0a9
F-Sepanski <sepanskifeliphe@gmail.com>
2025-05-01 23:17:33 -0300

    Tamanho dinamico de membros de diretorios

--- vinac/include/directory.h
+++ vinac/include/directory.h
@@ -4,7 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_MEMBROS 128
 #define MAX_NOME 1024
 
 typedef struct
@@ -21,11 +20,14 @@ typedef struct
 typedef struct
 {
   int total_membros;
-  MembroDir membros[MAX_MEMBROS];
+  int capacidade;
+  MembroDir *membros;
 } DiretorioArchive;
 
 // Inicializa o diretório vazio
+DiretorioArchive *criar_diretorio();
 void inicializar_diretorio(DiretorioArchive *dir);
+void liberar_diretorio(DiretorioArchive *dir);
 
 // Adiciona um membro ao diretório
 int adicionar_membro(DiretorioArchive *dir, const char *nome, int uid, long tamanho_original, long tamanho_disco, long data_modificacao, int ordem, long offset);
